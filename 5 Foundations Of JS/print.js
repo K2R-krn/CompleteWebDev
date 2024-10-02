ed5db80014fc82d069ede71d25af31f4f91919e5
Karansinh Rathod <krnsr.rthd@gmail.com>
2024-10-02 10:14:57 +0530

    practice

---
+++ 5 Foundations Of JS/print.js
@@ -0,0 +1,6 @@
+console.log("Hii man!! K2R is here yes baby the best !!"); // Gives out in new line
+
+process.stdout.write("K2R baby !!") // doesnt give new line
+console.log();
+
+console.table({city: "test"})
